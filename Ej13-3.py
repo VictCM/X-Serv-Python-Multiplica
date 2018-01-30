c94bcf00f41ee4b1a4ab595cc12a36c5f5715979
Victor Calzado Morales <vcalzado@gamma21.aulas.gsyc.urjc.es>
2018-01-30 10:41:34 +0100

    v1

--- Ej13-3.py
+++ Ej13-3.py
@@ -1,3 +1,10 @@
 #!/usr/bin/python3
 
-print("Hello Mundou")
+#Tabla Multiplicar
+
+Numbers = list(range(1,11))
+
+for number1 in Numbers:
+    for number2 in Numbers:
+        mult = number1 * number2
+        print(number1, "por", number2, "=", mult)
